ff33145662d8b441e4e1bbbb044ac382f7a9a96f
charlesfeatherstone <charlesfstone11@gmail.com>
2023-10-23 17:24:55 -0400

    done

--- lib/deli_counter.py
+++ lib/deli_counter.py
@@ -1,17 +1,36 @@
 katz_deli = []
 
-def line ():
-    pass
+def line (array):
+    integer=1
+    if array:
+        print("The line is currently: ", end="")
+        for name in array:
+            if name == array[len(array)-1]:
+               print(f"{integer}. {name}")
+            else:
+               print(f"{integer}. {name}",end=" ")
+               integer=integer+1
+
+    else:
+        print("The line is currently empty.")
 
 
-def take_a_number (curLine,name):
-    print(name,curLine)
+
+def take_a_number (array,name):
+    integer=1
+    array.append(name)
+    for element in array:
+        if element==name:
+            break
+        else:
+            integer=integer+1
+    print(f"Welcome, {name}. You are number {integer} in line.")
 
 
-def now_serving ():
-    if katz_deli[0] :
-        print(f'current list serving {katz_deli[0]}')
-        katz_deli.remove(katz_deli[0])
+def now_serving (array):
+    if array :
+        print(f'Currently serving {array[0]}.')
+        array.remove(array[0])
 
     else :
-        print ('there is nobody waiting to be served')
+        print ('There is nobody waiting to be served!')
